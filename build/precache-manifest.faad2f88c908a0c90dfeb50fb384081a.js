5dff69b8de66ad0afb824afe8ad34e459d946d35
anoandretta <54116273+anoandretta@users.noreply.github.com>
2019-10-02 15:21:02 +0200

    Add files via upload

---
+++ build/precache-manifest.faad2f88c908a0c90dfeb50fb384081a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb9f7ffe41ebb6fc5470c3e47fc72fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b5bacef0338463442781",
+    "url": "/static/css/main.76b6cc3e.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "b5bacef0338463442781",
+    "url": "/static/js/main.6a757372.chunk.js"
+  },
+  {
+    "revision": "31175d55b7ad255ef221",
+    "url": "/static/js/runtime~main.6237261c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
